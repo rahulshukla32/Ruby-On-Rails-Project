9b0b2caff54ed6fdf535193104b86a3cc334c750
Rahul Shukla <rahulshukla.nyu@gmail.com>
2018-07-02 15:33:58 -0400

    updated

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,7 +16,7 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
     if @article.update(article_params)
       flash[:notice] = "Article was successfully updated"
-      redirect_to_article_path(@article)
+      redirect_to article_path(@article)
     else
       render 'edit'
     end
@@ -26,7 +26,7 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
     if @article.save
       flash[:notice] = "Article was successfully created"
-      redirect_to_article_path(@article)
+      redirect_to article_path(@article)
     else
       render 'new'
     end
